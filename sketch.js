b978fde16d824282253315c821544b7d772cd623
Alessio De Nicolò <alessio1.denicolo@mail.polimi.it>
2024-03-17 17:25:26 +0100

    added controls and variables

--- sketch.js
+++ sketch.js
@@ -1,38 +1,42 @@
 function setup() {
-  createCanvas(500, 550);
-  frameRate(2.5)
-  //noLoop()
-pixelDensity(3);
+  createCanvas(1200, 640);
+  //frameRate(1)
+  noLoop()
+  pixelDensity(4);
 }
 
 function draw() {
   background(220);
-  
-  for (let i = 0; i <= height; i+=4) {
-    
-    let uno = random(100, 200);
-    let dos = random(300, 400);
-    let tres = random(400, 500);
-    
-    let a = random(0,uno); 
-    let b = random(a,uno);
-    let c = random(b, dos);
-    let d = random(c, dos);
-    let e = random(d, tres);
-    let f = random(e, tres);
-    stroke(50,95,209);
-    strokeWeight(4);
-    strokeCap(SQUARE);
-    line(a, i, b, i);
-    
-    stroke(50,95,209);
-    strokeWeight(4);
-    strokeCap(SQUARE);
-    line(c, i, d, i);
-    
-    stroke(50,95,209);
-    strokeWeight(4);
-    strokeCap(SQUARE);
-    line(e, i, f, i);
-  }
+  let pixelSize = 4; // how thick the lines are
+  let lineWidthVariation = 16; // how much the lines width can vary
+  let centralVariation = 4; // how much the lines are aligned to the center
+  let lineMinWidth = 8; // minimum line width
+  let lineMaxWidth = 64; // maximum line width
+  let centralPos = width / 2; // where the lines start
+  let lineWidth = int(random(lineMinWidth, lineMaxWidth + 1)) * pixelSize;
+
+  for (let i = 2; i <= height; i += pixelSize) {
+    let lineStart = centralPos - (lineWidth / 2) + centralOffset(centralVariation, pixelSize);
+    let lineEnd = lineStart + lineWidth;
+    drawLine(lineStart, lineEnd, i, pixelSize);
+    lineWidth = lineWidth + (getRandomPlusOrMinusOne() * int(random(0, lineWidthVariation)) * pixelSize);
+    centralPos = centralPos + getRandomPlusOrMinusOne() * centralVariation;
+  }
+}
+
+function drawLine (lineStart, lineEnd, i, pixelSize) {
+  stroke(50,95,209);
+  strokeWeight(pixelSize);
+  strokeCap(SQUARE);
+  line(lineStart, i, lineEnd, i);
+}
+
+function getRandomPlusOrMinusOne() {
+  let randomValue = random();
+  return randomValue < 0.5 ? -1 : 1;
+}
+
+function centralOffset(centralVariation, pixelSize) {
+  let offset = int(random(1, centralVariation));
+  return offset * getRandomPlusOrMinusOne() * pixelSize;
 }
