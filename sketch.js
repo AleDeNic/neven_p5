34ada6a0fae1925a3da798c6a2e771572184cf2d
Alessio De Nicolò <alessio1.denicolo@mail.polimi.it>
2024-03-17 20:00:52 +0100

    - removed pixelSize - made everything pixel perfect

--- sketch.js
+++ sketch.js
@@ -6,37 +6,40 @@ function setup() {
 }
 
 function draw() {
-  background(220);
-  let pixelSize = 4; // how thick the lines are
-  let centralPos = width / 2; // where the lines start
-  let lineWidthVariation = 32; // how much the lines width can vary
-  let centralVariation = 32; // how much the lines are aligned to the center
-  let fuzziness = 0; // how crazy the lines are
-  let minWidth = 16; // minimum line width
-  let maxWidth = 128; // maximum line width
+  background(200);
+  // set the dimension of the pixels
+  let scaleFactor = 4;
+  scale(scaleFactor);
+  // variables
+  let centralPos = width / scaleFactor / 2; // where the lines start
+  let lineWidthVariation = 4; // how much the lines width can vary
+  let centralVariation = 4; // how much the lines are aligned to the center
+  let fuzziness = 2; // how crazy the lines are
+  let minWidth = 4; // minimum line width
+  let maxWidth = 32; // maximum line width
   let lineWidth = int(random(minWidth, maxWidth + 1)); // first line width
-
-  for (let i = 2; i <= height; i += pixelSize) {
+  // draw the lines
+  for (let i = 1; i <= height / scaleFactor; i ++) {
     // calculate the start and end position of the line
-    let startPosition = centralPos - (lineWidth * pixelSize / 2) + getRandomPlusOrMinusOne() * random(0, fuzziness + 1) * pixelSize;
-    let endPosition = startPosition + lineWidth * pixelSize;
+    let startPosition = centralPos - int(lineWidth / 2) + plusMinus() * int(random(fuzziness + 1));
+    let endPosition = startPosition + lineWidth;
+    console.log(startPosition);
     // draw the line
-    drawLine(startPosition, endPosition, i, pixelSize);
+    drawLine(startPosition, endPosition, i);
     // update the line width and central position for the next line
-    lineWidth = lineWidth + getRandomPlusOrMinusOne() * int(random(lineWidthVariation + 1));
+    lineWidth = lineWidth + plusMinus() * int(random(lineWidthVariation + 1));
     lineWidth = constrain(lineWidth, minWidth, maxWidth);
-    centralPos = centralPos + getRandomPlusOrMinusOne() * int(random(centralVariation)) * pixelSize;
+    centralPos = centralPos + plusMinus() * int(random(centralVariation));
   }
 }
 
-function drawLine (startPosition, endPosition, i, pixelSize) {
+function drawLine (startPosition, endPosition, i) {
   stroke(30, 30, 210);
-  strokeWeight(pixelSize);
   strokeCap(SQUARE);
   line(startPosition, i, endPosition, i);
 }
 
-function getRandomPlusOrMinusOne() {
+function plusMinus() {
   let randomValue = random();
   return randomValue < 0.5 ? -1 : 1;
 }
